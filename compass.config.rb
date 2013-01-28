b90c74dcc99ce45615baf071ad40020f972f2a10
Alex Robson <alex@sharplearningcurve.com>
2013-01-28 12:18:21 -0600

    Break static content into jade files and setup anvil build for future development of the site.

---
+++ compass.config.rb
@@ -0,0 +1,13 @@
+line_comments = false
+output_style = :expanded
+# Here are some comments
+relative_assets = true # a comment
+
+sass_dir = 'src/sass' # Explanation here
+css_dir  = 'public/css'
+
+# javascripts_dir = 'lib/js'
+
+# images_dir = 'src/img'
+# generated_images_dir = 'lib/img'
+# Sass::Script::Number.precision = 8
